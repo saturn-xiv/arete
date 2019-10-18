da5bbcf1d3011072b7b2d7dcf90fd3aecd1f0cd3
Jeremy Zheng <jitang.zheng@gmail.com>
2019-10-17 21:04:48 -0700

    :wrench: remove dirty

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [ $# -ne 1 ] ; then
+    echo 'Please specify your libc type: glibc musl'
+    exit 1
+fi 
+
 export WORKSPACE=$PWD
 
 cd $WORKSPACE/dashboard
@@ -12,7 +17,17 @@ fi
 npm run build
 
 cd $WORKSPACE
-cargo build --release
+
+if [ $1 = 'glibc' ];
+then 
+    cargo build --release
+else if [ $1 = 'musl' ]
+    RUSTFLAGS="-C target-feature=-crt-static" cargo build --release
+else
+    echo "bad libc type $1"
+    exit 2
+fi
+
 strip -s target/release/arete
 
 exit 0
