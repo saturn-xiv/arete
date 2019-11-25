06324782cbd464dfc1691fc7a8feb7659d2234fa
Jeremy Zheng <jitang.zheng@gmail.com>
2019-11-24 21:51:35 -0800

    :arrow_up: upgrading dependencies.

---
+++ check.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+
+echo "Check default features..."
+cargo check
+echo "Build default features..."
+cargo build
+echo "Done."
+
+declare -a features=(
+    "mysql" 
+    "sqlite"    
+)
+
+for i in "${features[@]}"
+do
+    echo "Check features: $i..."
+    cargo check --no-default-features --features "$i"
+    echo "Build features: $i..."
+    cargo build --no-default-features --features "$i"
+    echo "Done."
+done
+
+exit 0
