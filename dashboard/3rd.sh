822454b8eddb530e6b5bb5823163656308dfe286
Jeremy Zheng <jitang.zheng@gmail.com>
2019-12-30 09:10:15 -0800

    :package: add third dependencies

---
+++ dashboard/3rd.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# MY
+npm install --save urijs @types/urijs \
+    react-helmet @types/react-helmet \
+    js-cookie  @types/js-cookie \
+    jwt-decode @types/jwt-decode \
+    moment-timezone react-moment \
+    react-quill react-markdown \
+    emoji-mart google-map-react \
+    qrcode.react react-copy-to-clipboard
