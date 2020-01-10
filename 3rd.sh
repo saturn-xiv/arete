31fd86598ce345297e01ed43be649f4bb6388e25
Jeremy Zheng <jitang.zheng@gmail.com>
2020-01-09 20:50:45 -0800

    :art: mv 3rd script to root folder

---
+++ 3rd.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# https://create-react-app.dev/docs/adding-typescript/
+# npx create-react-app dashboard --template typescript
+
+# third
+npm install --save urijs @types/urijs \
+    react-helmet @types/react-helmet \
+    js-cookie  @types/js-cookie \
+    jwt-decode @types/jwt-decode \
+    @loadable/component @types/loadable__component \
+    redux react-redux \
+    react-router-dom @types/react-router-dom \
+    moment moment-timezone react-moment \
+    react-intl react-quill react-markdown \
+    emoji-mart google-map-react \
+    qrcode.react react-copy-to-clipboard antd
