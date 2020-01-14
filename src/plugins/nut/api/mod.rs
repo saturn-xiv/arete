19710e29088282735e4f607a2ec26bcad61428e2
Jeremy Zheng <jitang.zheng@gmail.com>
2020-01-13 16:17:59 -0800

    :bug: fix title & subhead show

--- src/plugins/nut/api/mod.rs
+++ src/plugins/nut/api/mod.rs
@@ -9,8 +9,9 @@ use chrono::{NaiveDateTime, Utc};
 use super::super::super::{
     env::{AUTHORS, BUILD_TIME, DESCRIPTION, HOMEPAGE, NAME, VERSION},
     errors::Result,
-    i18n,
+    i18n::{self, I18n},
     orm::Pool as Db,
+    request::Locale,
 };
 
 #[post("/install")]
@@ -19,16 +20,21 @@ async fn install() -> impl Responder {
 }
 
 #[get("/about")]
-async fn about(db: web::Data<Db>) -> Result<impl Responder> {
+async fn about(lang: Locale, db: web::Data<Db>) -> Result<impl Responder> {
     let db = db.get()?;
     let db = db.deref();
     let languages = i18n::locale::Dao::languages(db)?;
+    let lang = lang.0;
+
     Ok(HttpResponse::Ok().json(json!({
         "name": NAME,
         "version": VERSION,
         "build": BUILD_TIME,
         "homepage": HOMEPAGE,
         "authors": AUTHORS,
+        "title": I18n::t(db, &lang, "site.title", &None::<String>),
+        "subhead": I18n::t(db, &lang, "site.subhead", &None::<String>),
+        "copyright": I18n::t(db, &lang, "site.copyright", &None::<String>),
         "description": DESCRIPTION,
         "languages": languages,
     })))
