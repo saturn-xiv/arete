b8c7ed44550921833bcc7e57c4f27e92beccdd9c
Jeremy Zheng <jitang.zheng@gmail.com>
2020-01-25 21:51:41 -0800

    :bug: using jsonwebtoken v7

--- src/jwt.rs
+++ src/jwt.rs
@@ -1,7 +1,9 @@
 use std::ops::Add;
 
 use chrono::{Duration, Utc};
-use jsonwebtoken::{decode, encode, Algorithm, Header, TokenData, Validation};
+use jsonwebtoken::{
+    decode, encode, Algorithm, DecodingKey, EncodingKey, Header, TokenData, Validation,
+};
 use serde::{de::DeserializeOwned, ser::Serialize};
 
 use super::errors::Result;
@@ -27,13 +29,17 @@ impl Jwt {
         let mut header = Header::default();
         header.kid = kid;
         header.alg = Algorithm::HS512;
-        let token = encode(&header, claims, self.key.as_bytes())?;
+        let token = encode(
+            &header,
+            claims,
+            &EncodingKey::from_base64_secret(&self.key)?,
+        )?;
         Ok(token)
     }
     pub fn parse<T: DeserializeOwned>(&self, token: &String) -> Result<TokenData<T>> {
         let mut vat = Validation::new(Algorithm::HS512);
         vat.leeway = 60;
-        let val = decode(token, self.key.as_bytes(), &vat)?;
+        let val = decode(token, &DecodingKey::from_base64_secret(&self.key)?, &vat)?;
         Ok(val)
     }
 }
