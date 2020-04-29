9463247a50ffdbb5ceaff9e0c6b696669287092c
Jeremy Zheng <jitang.zheng@gmail.com>
2020-04-28 23:32:41 -0700

    :art: using umijs

--- 3rd.sh
+++ 3rd.sh
@@ -5,17 +5,19 @@
 
 # third
 npm install --save urijs @types/urijs \
-    react-helmet @types/react-helmet \
     js-cookie  @types/js-cookie \
     jwt-decode @types/jwt-decode \
     dinero @types/dinero.js \
-    @loadable/component @types/loadable__component \
-    redux react-redux @types/react-redux \
-    react-router-dom @types/react-router-dom \
     moment moment-timezone react-moment @types/moment-timezone \
-    react-intl react-quill \
-    react-mde react-markdown \
+    react-quill react-mde react-markdown \
     emoji-mart google-map-react \
     qrcode.react react-copy-to-clipboard \
     famfamfam-flags famfamfam-mini famfamfam-silk \
-    @material-ui/core @material-ui/icons @material-ui/pickers
+    @fluentui/react react-hook-form
+    
+# @material-ui/core @material-ui/icons @material-ui/pickers
+    #react-intl 
+    #redux react-redux @types/react-redux \
+    #react-router-dom @types/react-router-dom \
+    #@loadable/component @types/loadable__component \
+    #react-helmet @types/react-helmet \
