16a653deb6807da29498530d1f10f09593e4cf39
Jeremy Zheng <jitang.zheng@gmail.com>
2020-05-15 23:27:14 -0700

    :heavy_plus_sign: add frontend packages

--- tools/3rd.sh
+++ tools/3rd.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # https://create-react-app.dev/docs/adding-typescript/
-# npx create-react-app dashboard --template typescript
+# yarn create create-react-app dashboard --template typescript
 
 # third
-npm install --save urijs @types/urijs \
+yarn add urijs @types/urijs \
     js-cookie  @types/js-cookie \
     jwt-decode @types/jwt-decode \
     dinero @types/dinero.js \
@@ -13,11 +13,9 @@ npm install --save urijs @types/urijs \
     emoji-mart google-map-react \
     qrcode.react react-copy-to-clipboard \
     famfamfam-flags famfamfam-mini famfamfam-silk \
-    @fluentui/react react-hook-form
-    
-# @material-ui/core @material-ui/icons @material-ui/pickers
-    #react-intl 
-    #redux react-redux @types/react-redux \
-    #react-router-dom @types/react-router-dom \
-    #@loadable/component @types/loadable__component \
-    #react-helmet @types/react-helmet \
+    react-intl \
+    redux react-redux @types/react-redux \
+    react-router-dom @types/react-router-dom \
+    @loadable/component @types/loadable__component \
+    react-helmet @types/react-helmet \
+    @fluentui/react \
