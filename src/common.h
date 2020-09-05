239eaf5870009fa6f7a27e0ffa71c768e24e2a6e
Jeremy Zheng <jitang.zheng@gmail.com>
2020-09-05 16:50:44 -0700

    :construction: add minium main

---
+++ src/common.h
@@ -0,0 +1,8 @@
+#ifndef ARETE_COMMON_H_
+#define ARETE_COMMON_H_
+
+#include <iostream>
+
+#include "config.h"
+
+#endif
---
+++ src/main.cc
@@ -0,0 +1,7 @@
+#include "common.h"
+
+int main()
+{
+    std::cout << "Version " << ARETE_VERSION << std::endl;
+    return 0;
+}
