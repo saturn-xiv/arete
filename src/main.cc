a655f310bb454610258703a1d97eda2ca60fbaa2
Jeremy Zheng <jitang.zheng@gmail.com>
2020-09-05 18:16:18 -0700

    :construction: add build time & git version

--- src/main.cc
+++ src/main.cc
@@ -4,7 +4,7 @@
 int main()
 {
 
-    BOOST_LOG_TRIVIAL(info) << "Arete(" << ARETE_VERSION << ") Boost(v" << (BOOST_VERSION / 100000) << "." << ((BOOST_VERSION / 100) % 1000) << "." << (BOOST_VERSION % 100) << ")";
+    BOOST_LOG_TRIVIAL(info) << "Arete(" << ARETE_GIT_VERSION << " " << __TIMESTAMP__ ") Boost(v" << (BOOST_VERSION / 100000) << "." << ((BOOST_VERSION / 100) % 1000) << "." << (BOOST_VERSION % 100) << ")";
 
     BOOST_LOG_TRIVIAL(warning) << "Exit.";
 
